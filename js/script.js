5b2640108a4352169668a4d9027e89082008e0c5
YoanHalevy <95260462+YoanHalevy@users.noreply.github.com>
2022-01-26 16:55:01 -0500

    finalizing changes

--- js/script.js
+++ js/script.js
@@ -2,13 +2,8 @@
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
-
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /*** 
- * `quotes` array 
+ * Building an array called "quotes" to store the quotes 
 ***/
 var quotes = [
   {
@@ -27,7 +22,8 @@ var quotes = [
     quote: "I was never one to hold a grudge. My father held grudges, I’ll always hate him for that.",
     source: "Pierce Hawthorne",
     citation: "Community",
-    year: "2009"
+    year: "2009",
+    tags: "Humor, Irony, Tv Series"
   },
   {
     quote: "You see, Jeff, there are certain things man was not meant to hear. We were designed by whatever entity you choose to hear what's in this range, and really this range alone. Because you know who's talking to us in this range? The people we love.",
@@ -43,36 +39,32 @@ var quotes = [
   }
 ]
 /***
- * `getRandomQuote` function
+ * creating a function to generate a random number and pull a corresponding quote from the array
 ***/
 function getRandomQuote() {
-  // 1. Create a variable that generates a random number
-  // between zero and the last index in the `quotes` array
   var randomNum = Math.floor(Math.random() * quotes.length);
-  // 2. Use the random number variable and bracket notation 
-  // to grab a random object from the `quotes` array, and 
-  // store it in a variable
   var randomQuote = quotes[randomNum];
-  // 3. Return the variable storing the random quote object
   return randomQuote;
 }
 /***
- * `printQuote` function
+ * creating a function that uses the pulled quote to print it to the page
 ***/
 function printQuote() {
-var rQO = getRandomQuote();  
-var htmlString = `<p class="quote"> ${rQO.quote} </p><p class="source"> ${rQO.source} `;
-if (rQO.citation) {
-  htmlString += `<span class="citation"> ${rQO.citation} </span>`;
+  var rQO = getRandomQuote();  
+  var htmlString = `<p class="quote">${rQO.quote}</p><p class="source">${rQO.source}`;
+  if (rQO.citation) {
+    htmlString += `<span class="citation">${rQO.citation}</span>`;
+  }
+  if (rQO.year) {
+    htmlString += `<span class="year">${rQO.year}</span>`;
+  }
+  if (rQO.tags) {
+    htmlString += `<span class="year">${rQO.tags}</span>`;
+  }
+  htmlString += `</p>`;
+  return document.getElementById('quote-box').innerHTML = htmlString; 
 }
-if (rQO.year) {
-  htmlString += `<span class="year"> ${rQO.year} </span>`;
-}
-htmlString += `</p>`;
-return document.getElementById('quote-box').innerHTML = htmlString; 
-}
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+// Setting a 10 second refresh rate for the quotes
+setInterval(printQuote, 10000);
+
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
